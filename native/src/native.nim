e8f3cc7f126e58b3b0159b2870d8768ab01fb193
ryznxx <ryuzenfarizi@gmail.com>
2025-07-01 00:19:45 +0700

    big changes

--- native/src/native.nim
+++
@@ -1,9 +0,0 @@
-import asynchttpserver, asyncdispatch
-import handlers
-include "./config/config"
-
-
-# should be native http handler if using node is danger
-let server = newAsyncHttpServer()
-
-waitFor server.serve(Port(5000), handler)
--- native/src/utils/reader.nim
+++
@@ -1,3 +0,0 @@
-proc readStr*(location: string): string =
-   let output: string = readFile(location)
-   return output
